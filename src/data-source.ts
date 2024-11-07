5903e20f4cf0266c5c28f8ffde9e2c375f589f18
John L <lazaro4958@saskpolytech.ca>
2024-11-07 17:36:45 -0600

    Added the new entities to the data source file

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,13 +2,15 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 import { Employee } from "./entity/Employee"
+import {Shift} from "./entity/Shift";
+import {Department} from "./entity/Department";
 
 export const AppDataSource = new DataSource({
     type: 'sqlite',
     database: './sqlite.db',
     synchronize: true,
     logging: false,
-    entities: [User, Employee],
+    entities: [User, Employee, Department, Shift],
     migrations: [],
     subscribers: [],
 })
