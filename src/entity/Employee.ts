cb520f5544993683f3496afaaca743908f2a85f4
John <John.e.lazaro@gmail.com>
2024-11-13 18:55:16 -0600

    Created validator statements for the employee object

--- src/entity/Employee.ts
+++ src/entity/Employee.ts
@@ -1,22 +1,32 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm"
+import { IsNotEmpty, IsOptional, Max, Min } from "class-validator"
+import {Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn} from "typeorm"
 
 @Entity()
 export class Employee {
 
     @PrimaryGeneratedColumn()
+    @IsOptional()
     id: string
 
     @Column()
+    @IsNotEmpty({ message: "The first name is required" })
     firstName: string
 
     @Column()
+    @IsNotEmpty({ message: "The last name is required" })
     lastName: string
 
     @Column()
+    @IsOptional()
+    @Max(40, { message: "The max amount of requested hours of an employee cannot exceed 40" })
+    @Min(3, { message: "The minimum of hours that must be worked is 3 hours in a business week" })
     maxHours: number
 
     // This is going to be a foreign key that references another employee
     @Column()
-    managedBy: number
+    @OneToOne(type => Employee)
+    @JoinColumn({ name: "managerID" })
+    @IsOptional()
+    managedBy: Employee
 
 }
