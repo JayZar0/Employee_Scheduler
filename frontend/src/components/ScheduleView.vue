5b82fd13636da33af1cdad333f2aa999c0972d95
John L <lazaro4958@saskpolytech.ca>
2024-11-20 15:03:03 -0600

    created schdule component to display the calendar and the schedule list.

---
+++ frontend/src/components/ScheduleView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+import { Calendar } from 'primevue/calendar'
+
+const isManager = ref(false)
+</script>
+
+<template>
+  <Calendar inline showWeek />
+</template>
+
+<style scoped>
+
+</style>
