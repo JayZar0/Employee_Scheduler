2d651c760bbf7a879b18903404bf6e79d41356b9
John L <lazaro4958@saskpolytech.ca>
2024-11-21 09:41:17 -0600

    Got the calendar component to work

--- frontend/src/components/ScheduleView.vue
+++ frontend/src/components/ScheduleView.vue
@@ -1,14 +1,22 @@
 <script setup>
 import { ref } from 'vue'
-import { Calendar } from 'primevue/calendar'
+import Calendar from 'primevue/calendar'
+import Listbox from 'Primevue/listbox'
 
 const isManager = ref(false)
+const date = ref()
 </script>
 
 <template>
-  <Calendar inline showWeek />
+  <div class="container">
+    <Calendar v-model="date" inline />
+    <Listbox></Listbox>
+  </div>
 </template>
 
 <style scoped>
-
+.container {
+  display: grid;
+  grid-template-columns: auto 1fr;
+}
 </style>
