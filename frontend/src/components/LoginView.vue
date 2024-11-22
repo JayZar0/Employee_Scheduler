e3a64c5684b20fb29d3201a6be4fc2f202b7b639
Patreece Eichel <patreece.eichel@outlook.com>
2024-11-22 11:54:55 -0600

    login fields

--- frontend/src/components/LoginView.vue
+++ frontend/src/components/LoginView.vue
@@ -1,13 +1,24 @@
 <script setup>
 import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import { ref } from 'vue';
+
+const value = ref(null);
 
 </script>
 
 <template>
   <h2>Test</h2>
+
+  <template>
+    <div class="card flex justify-center">
+      <InputText type="text" v-model="value" />
+    </div>
+  </template>
+
+  <InputText type="text" v-model="value" />
+  <InputText type="text" v-model="value" />
   <Button label="login"></Button>
-
-
 </template>
 
 <style scoped>
