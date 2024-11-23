ddd056c45685439b4e63d868c1d4f5252d8d6154
John <John.e.lazaro@gmail.com>
2024-11-22 22:02:43 -0600

    created new shift form and modal window for the shift form

--- frontend/src/components/ScheduleView.vue
+++ frontend/src/components/ScheduleView.vue
@@ -1,13 +1,17 @@
 <script setup>
 import { ref } from 'vue'
 import { DatePicker } from 'primevue'
-import Listbox from 'primevue/listbox'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 import Dialog from 'primevue/dialog'
+import Button from 'primevue/button'
+import ShiftForm from "./ShiftForm.vue";
 
 const isManager = ref(true)
 const date = ref()
 const selectedShift = ref()
 const schedule = ref()
+const visible = ref(false)
 
 async function getShifts() {
   const shiftsFromDB = await fetch(`/api/shifts`, {
@@ -29,6 +33,19 @@ getShifts()
 <template>
   <div class="container">
     <div class="left">
+      <Button @click="visible = true" label="Add Shift" />
+      <Dialog v-model:visible="visible" modal header="New Shift"
+              :style="{
+                'background-color': 'grey',
+                 height: '30vh',
+                 width: '30vw',
+                 padding: '10px',
+                 'border-radius': '10px',
+                 filter: 'drop-shadow(0 0 0.75rem crimson)'
+              }"
+      >
+        <ShiftForm />
+      </Dialog>
       <DatePicker v-model="date" inline class="calendar" />
     </div>
     <div class="right">
@@ -36,7 +53,13 @@ getShifts()
         <option value="test">Test Value</option>
         <option value="test2">Test Value#2</option>
       </select>
-      <Listbox v-model="selectedShift" :options="schedule" optionLabel="startHour" />
+      <DataTable :value="schedule">
+        <Column field="employeeID" header="Name"></Column>
+        <Column field="departmentID" header="Department"></Column>
+        <Column field="day" header="Date"></Column>
+        <Column field="startHour" header="Start"></Column>
+        <Column field="endHour" header="End"></Column>
+      </DataTable>
     </div>
   </div>
 </template>
@@ -58,6 +81,8 @@ getShifts()
   flex: 1 1 auto;
   margin: 30px;
   padding: 10px;
+  background-color: black;
+  border-radius: 15px;
 }
 
 .right {
@@ -65,6 +90,7 @@ getShifts()
   margin: 30px;
   padding: 10px;
   border: 3px solid white;
+  border-radius: 15px;
 }
 
 @media (prefers-color-scheme: light) {
---
+++ frontend/src/components/ShiftForm.vue
@@ -0,0 +1,37 @@
+<script setup>
+import FloatLabel from 'primevue/floatlabel'
+import InputNumber from 'primevue/inputnumber'
+import Listbox from 'primevue/listbox'
+import DatePicker from 'primevue/datepicker'
+import Button from 'primevue/button'
+
+</script>
+
+<template>
+  <form action="/api/shifts">
+    <Listbox />
+
+    <Listbox />
+
+    <FloatLabel variant="on">
+      <DatePicker id="date" />
+      <label for="date">Date</label>
+    </FloatLabel>
+
+    <FloatLabel variant="on">
+      <InputNumber id="startTime" :min="1" :max="24" showButtons buttonLayout="horizontal" />
+      <label for="startTime">Start</label>
+    </FloatLabel>
+
+    <FloatLabel variant="on">
+      <InputNumber id="endTime" :min="1" :max="24" showButtons buttonLayout="horizontal" />
+      <label for="endTime">End</label>
+    </FloatLabel>
+
+    <Button label="submit" type="submit" />
+  </form>
+</template>
+
+<style scoped>
+
+</style>
