1aa5860313d6926ca621a75220579fcc0a67fb94
Patreece Eichel <patreece.eichel@outlook.com>
2024-11-27 13:25:33 -0600

    login form FE validation

--- frontend/src/components/LoginView.vue
+++ frontend/src/components/LoginView.vue
@@ -10,13 +10,19 @@ import InputText from 'primevue/inputtext';
 
 const toast = useToast();
 const initialValues = ref({
-  password: ''
+  password: '',
+  email: ''
 });
+
+// Define separate validation for email and password
+const emailValidation = z.string().min(1, { message: 'Email is required' }).email({ message: 'Please enter a valid email' });
+const passwordValidation = z.string().min(1, { message: 'Password is required' });
+
+// Define the resolver correctly for the form
 const resolver = ref(zodResolver(
     z.object({
-      password: z
-          .string()
-          .min(1, { message: 'Password is required' })
+      email: emailValidation,
+      password: passwordValidation
     })
 ));
 
@@ -31,22 +37,27 @@ const onFormSubmit = ({ valid }) => {
   <h2>Login</h2>
   <Form v-slot="$form" :resolver="resolver" :initialValues="initialValues" @submit="onFormSubmit" class="flex flex-col gap-4 w-full sm:w-64 items-center">
     <div class="flex flex-col gap-1">
-      <InputText type="text" v-model="value" />
-      <div class="password-container">
+      <div class="input-container">
+        <InputText name="email" class="text-input" type="text" v-model="value" placeholder="Email"/>
+        <template v-if="$form.email?.invalid">
+          <Message :key="index" severity="error" size="small" variant="simple" class="error-message">{{ $form.email.errors[0].message }}</Message>
+        </template>
         <Password name="password" placeholder="Password" :feedback="false" fluid toggle-mask/>
+        <template v-if="$form.password?.invalid">
+          <Message v-for="(error, index) in $form.password.errors" :key="index" severity="error" size="small" variant="simple" class="error-message">{{ error.message }}</Message>
+        </template>
       </div>
-      <template v-if="$form.password?.invalid">
-        <Message v-for="(error, index) in $form.password.errors" :key="index" severity="error" size="small" variant="simple" class="error-message">{{ error.message }}</Message>
-      </template>
+
     </div>
-    <Button type="submit" severity="secondary" label="Submit" />
+    <Button type="submit" severity="primary" label="Submit" />
   </Form>
 </template>
 
 <style>
-.password-container {
+.input-container {
   width: 200px; /* Adjust the width as needed */
   display: flex;
+  flex-direction: column;
   justify-content: center;
   margin: 10px;
 }
@@ -61,4 +72,9 @@ form {
   color: #e45555; /* Set the error message text color to red */
   margin-bottom: 10px;
 }
+
+.text-input {
+  margin-bottom: 10px;
+}
+
 </style>
