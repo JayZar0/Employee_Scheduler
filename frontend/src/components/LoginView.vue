622ba3a9c4e06ef27be4c87bae070154d8d1ca10
Patreece Eichel <patreece.eichel@outlook.com>
2024-11-29 11:25:30 -0600

    email and password can be verified, need to adjust dialog components to dynamically populate email address

--- frontend/src/components/LoginView.vue
+++ frontend/src/components/LoginView.vue
@@ -14,14 +14,24 @@ import InputText from 'primevue/inputtext';
 import InvalidPasswordView from './InvalidPassword.vue'
 import InvalidEmailView from './InvalidEmailView.vue'
 
-// validation for the email & password
+// References to email and password
+const email = ref('');
+const password = ref('');
+
+// Visible properties for the error messages
+const invalidPasswordVisible = ref(false);
+const invalidEmailVisible = ref(false);
+
+// Validation for the email & password
 const emailValidation = z
                         .string()
                         .min(1, { message: 'Email is required' })
                         .email({ message: 'Please enter a valid email' });
+
 const passwordValidation = z
                           .string()
                           .min(1, { message: 'Password is required' });
+
 
 // Resolver for checking form objects
 const resolver = ref(zodResolver(
@@ -31,42 +41,22 @@ const resolver = ref(zodResolver(
     })
 ));
 
-// Visible properties for the error messages
-const invalidPasswordVisible = ref(false);
-const invalidEmailVisible = ref(false);
-
-/**
- * Searches for an employee in the database based on their email address
- * @param email - the email address entered in the form
- * @returns {Promise<void>}
- */
-function validateEmail(email) {
-  let validEmail = false;
-
-  //TODO: Implement, make valid to true
-
-
-  return validEmail;
-}
-
 /**
  * Given a valid, existing email we check that the password matches what is stored in the DB
  * @param email the email of the user logging in
- * @param password the password the user is trying to login with
+ * @param password the password the user is trying to log in with
  */
-function validatePassword(email, password) {
-  let validPassword = false;
-
-  //TODO: Implement, change valid to true
-
-  return validPassword;
+function validatePassword(emp) {
+  // check the entered password against the stored password for this email
+  return password.value === emp.password;
 }
 
 /**
  * Searches the DB for a user with the email provided in the login screen
+ * @param email the email address that the user is trying to login with
  * @returns {Promise<void>}
  */
-async function getEmployeeByEmail() {
+function getEmployeeByEmail(email) {
 
   // define options object for getting the employees
   const options = {
@@ -74,84 +64,88 @@ async function getEmployeeByEmail() {
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
-      Authorization: `MANAGER_KEY`
+      Authorization: `MANAGER_KEY` //TODO: change this ??
     }
   }
 
-  // TODO: email should come from entered email
-
-  const empPromise = await fetch(`api/employees`, options);
-
-  //const empPromise = await fetch(`api/employees?search=${email}`, options);
-  const empsFromDB = await empPromise.json();
-  console.log(empsFromDB);
-
-  //TODO: check if an employee is returned
+  // Let's search for the employee in the database given the email entered
+  return fetch(`api/employees?search=${email}`, options)
+      .then(response => response.json())
+      // emp will be in an array, pull out emp object or return null if none is found
+      .then(empsFromDB => empsFromDB[0] ? empsFromDB[0] : null);
 }
 
 /**
  * Method for checking if the email and password match an employee in the DB
  */
 function authenticate() {
+  // try to look up the employee, this will be async
+  const matchedEmp = getEmployeeByEmail(email.value) // this uses the email ref
+      .then(matchedEmp => {
+        // let's see if there is an employee with that email
+        if (matchedEmp) {
+          // check the entered password vs. the stored password
+          if (validatePassword(matchedEmp)) {
+            // redirect if it matches
+            // TODO: redirect
+          }
+          // show password error dialog if it doesn't match what is in the DB
+          else {
+            invalidPasswordVisible.value = true;
+          }
 
-  // try to look up the employee
-  const matchedEmp = getEmployeeByEmail();
-
-  // try to GET the user based on their email
-  if (!validateEmail(matchedEmp.email)) {
-    // show the error modal if the email is not associated with a stored employee
-    invalidEmailVisible.value = true;
-  }
-
-  else {
-    // valid email, let's check the password
-    if (!validatePassword(matchedEmp.email, matchedEmp.password)) {
-      // display error modal if the password does not match the email
-      invalidPasswordVisible.value = true;
-    }
-  }
+        }
+        // if no emp is found with that email show appropriate error dialog
+        else {
+          invalidEmailVisible.value = true;
+          //TODO: WE NEED TO PASS email TO THIS COMPONENT
+        }
+      });
 }
-// on submit function validates the user and provides them with their bearer token
-const onFormSubmit = ({ valid }) => {
- console.log("Login submitted");
- authenticate(); // look up the users credentials, give appropriate bearer token or alert them of error
+
+
+/**
+ * On submit authenticates the user trying to login
+ */
+const onFormSubmit = () => {
+  authenticate(); // look up the users credentials, give appropriate bearer token or alert them of error
 };
-
-
 </script>
 
+
 <template>
-
-
   <!-- Login form using email and password -->
   <h2>Login</h2>
+
   <Form v-slot="$form" :resolver="resolver" :initialValues="initialValues" @submit="onFormSubmit" class="flex flex-col gap-4 w-full sm:w-64 items-center">
     <div class="flex flex-col gap-1">
       <div class="input-container">
 
         <!-- Email input -->
-        <InputText name="email" class="text-input" type="text" v-model="value" placeholder="Email"/>
+        <InputText name="email" class="text-input" type="text" v-model="email" placeholder="Email"/>
         <template v-if="$form.email?.invalid">
           <Message :key="index" severity="error" size="small" variant="simple" class="error-message">{{ $form.email.errors[0].message }}</Message>
         </template>
 
         <!-- Password input -->
-        <Password name="password" placeholder="Password" :feedback="false" fluid toggle-mask/>
+        <Password name="password" placeholder="Password" :feedback="false" v-model="password" fluid toggle-mask/>
         <template v-if="$form.password?.invalid">
           <Message v-for="(error, index) in $form.password.errors" :key="index" severity="error" size="small" variant="simple" class="error-message">{{ error.message }}</Message>
         </template>
       </div>
 
     </div>
+    <!-- Submit button triggers authentication -->
     <Button type="submit" severity="primary" label="Submit" />
   </Form>
 
   <!--Shown if an invalid password is entered -->
   <InvalidPasswordView v-model:visible="invalidPasswordVisible"/>
 
+  <!--Shown if an email that doesn't correspond to an employee is entered-->
   <InvalidEmailView v-model:visible="invalidEmailVisible"/>
-
 </template>
+
 
 <style>
 .input-container {
@@ -176,5 +170,4 @@ form {
 .text-input {
   margin-bottom: 10px;
 }
-
 </style>
