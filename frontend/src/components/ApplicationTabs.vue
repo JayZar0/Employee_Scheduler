7325ea1a913b8c4e267f57b74254a96cd0e15e3e
John <lazaro4958@saskpolytech.ca>
2024-12-11 09:12:22 -0600

    tabs are now working

--- frontend/src/components/ApplicationTabs.vue
+++ frontend/src/components/ApplicationTabs.vue
@@ -23,14 +23,14 @@
   <div class="card">
     <Tabs value="/dashboard">
       <TabList>
-        <Tab v-for="tab in items" :key="tab.label" :value="tab.route">
-          <router-link v-if="tab.route" v-slot="{ href, navigate }" :to="tab.route" custom>
-            <a v-ripple :href="href" @click="navigate" class="flex items-center gap-2 text-inherit">
+        <router-link v-for="tab in items" :key="tab.label" :to="tab.route">
+          <Tab :value="tab.route">
+<!--            <a v-ripple :href="href" @click="navigate" class="flex items-center gap-2 text-inherit">-->
               <i :class="tab.icon" />
               <span class="navText">{{ tab.label }}</span>
-            </a>
-          </router-link>
-        </Tab>
+<!--            </a>-->
+          </Tab>
+        </router-link>
       </TabList>
     </Tabs>
   </div>
